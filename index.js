411d166a8a300ecfbecb10914ec0d7ff6b56cdbe
SemenVologdin <thestrikesnake@gmail.com>
2021-09-08 00:25:06 +0500

    create functional

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
-const $buttons = document.querySelectorAll('.card')
-const $rows = document.querySelector('.row')
-const $selectedCards = document.querySelectorAll('.front')
+const $card = document.querySelectorAll('.card');
+const $rows = document.querySelector('.row');
+const $selectedCards = document.querySelectorAll('.front');
+const $cards = document.querySelector('.cards');
 
-let countPics = 0
-const emogies = ['🐭','🐹','🦄','🐸','🐵','🐷','🐭','🐹','🦄','🐸','🐵','🐷']
-const sortArr = shuffle(emogies)
+const emogies = ['🐭', '🐹', '🦄', '🐸', '🐵', '🐷', '🐭', '🐹', '🦄', '🐸', '🐵', '🐷'];
+const sortArr = shuffle(emogies);
+const buttons = [];
 
-let firstPic 
-let secondPic
+let firstPic,
+  secondPic,
+  flipped = false;
 
 //Shuffle array for random pictures
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
 
-//Add listener for card and add picture in card
-$buttons.forEach((button,index)=>{
-  button.childNodes[3].innerHTML==''?button.childNodes[3].innerHTML=sortArr[index]:null
-  button.addEventListener('click',()=>{
-    button.classList.toggle('active')
-    console.log(index, sortArr[index])
-  })
-})
+//Add picture in card
+$card.forEach((button, index) => {
+  button.childNodes[3].innerHTML == '' ? (button.childNodes[3].innerHTML = sortArr[index]) : null;
+});
 
-function check() {
-  
+function unflipCards() {
+  firstPic.querySelector('.front').classList.add('wrong');
+  secondPic.querySelector('.front').classList.add('wrong');
+  setTimeout(() => {
+    firstPic.classList.remove('active');
+    secondPic.classList.remove('active');
+  }, 1000);
+
+  firstPic.querySelector('.front').classList.remove('wrong');
+  secondPic.querySelector('.front').classList.remove('wrong');
 }
 
+function disableCard() {
+  firstPic.removeEventListener('click', flipCard);
+  secondPic.removeEventListener('click', flipCard);
+  firstPic.querySelector('.front').classList.add('right');
+  secondPic.querySelector('.front').classList.add('right');
+}
 
+$card.forEach((card) => card.addEventListener('click', flipCard));
 
-
-
-
-
+function flipCard() {
+  this.classList.add('active');
+  if (!flipped) {
+    firstPic = this;
+    flipped = true;
+  } else {
+    flipped = false;
+    secondPic = this;
+    firstPic.querySelector('.front').innerHTML == secondPic.querySelector('.front').innerHTML
+      ? disableCard()
+      : unflipCards();
+  }
+}
